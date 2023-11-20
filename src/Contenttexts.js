1b07a1d7035665cd490edbdda8d49f1652a5a51a
Timo Petzold <tpetzol2@hs-mittweida.de>
2023-11-20 19:34:23 +0100

    fixed texts again

--- src/Contenttexts.js
+++ src/Contenttexts.js
@@ -1,21 +1,22 @@
 export const WWDescription = () => {
-    return "Tool mit einer Gerätedatenbank, Accountverwaltung, Projekterstellung, Datenim- und Export, Forum, Chatbot mit ChatGPT-API, Shopanbindung." +
+    return "Tool mit einer Gerätedatenbank, Accountverwaltung, Projekterstellung, Datenim- und Export, Forum, Chatbot mit ChatGPT-API, Shop." +
         " 7 Personen Team aufgeteilt in Tech und Design haben 1,5 Jahre an diesem Prototyp gearbeitet, im Auftrag eines Kunden.";
 }
 
 export const BetaDescription = () => {
-    return "70 Stundenten arbeiten zwei Semester lang in den Departments Gamedesign, Art, Tech, Engine und Eventmanagement daran ein Spiel zu entwickeln." +
+    return "70 Studenten arbeiten zwei Semester lang in den Departments Gamedesign, Art, Tech, Engine und Eventmanagement daran ein Spiel zu entwickeln." +
         " Dabei wird das Spiel Echolight entstehen, ein 3D Roque Like Dungeon Crawler mit einer isometrischen Perspektive und einem ästhetischem Fokus auf Licht und Dunkelheit."
 }
 
 export const HotlineDescription = () => {
-    return "Ein Spiel, welches die Hotline Miami Reihe parodiert. Es wurde im Rahmen des Moduls Gameprogramming umgesetzt." +
+    return "Ein Spiel, welches die Hotline Miami Reihe parodiert. " +
+        "Es wurde im Rahmen des Moduls Gameprogramming umgesetzt." +
         " Dabei ist zu erwähnen das sich dabei nur auf die technische Umsetzung konzentriert wurde.";
 }
 
 export const WarehouseDescription = () => {
     return "Gestaltet als Lehrspiel, hat dieses Projekt zum Ziel, dem Spieler das Konzept des Hashings näher zu bringen. " +
-        "Dabei lernt der Spieler verschiedene Hashing-Verfahren kennen und kann diese anwenden wenn er Waren in ein Regal einräumt oder aus einem Regal entnimmt." +
+        "Dabei lernt der Spieler verschiedene Hashing-Verfahren kennen und kann diese anwenden wenn er Waren in ein Regal einräumt oder aus einem Regal entnimmt. " +
         "Entwickelt wurde es im Rahmen des Algorithmen- und Datenstrukturen-Moduls über 1 Semester, mit einem Team aus 7 Personen bestehend aus Programmieren, Designern und 3D-Artists. "
 
 
